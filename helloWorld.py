375a7eda3a439870e6ecdac1a7b867973207e05a
Pedro Paez <peterpaez@Pedros-MacBook-Pro.local>
2024-03-17 00:30:37 +0000

    original message changed to, Git is Awesome

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+class HelloWorld:
+    def print_message(self):
+        print("Git is Awesome!")  
+
+# Creating an instance of the HelloWorld class
+hello = HelloWorld()
+
+# Calling the print_message method of the instance
+hello.print_message()
